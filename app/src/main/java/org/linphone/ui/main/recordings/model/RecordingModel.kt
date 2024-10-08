d547f57f9b8209948d0d95049175a2434576fb28
mineruking <kmin010287@gmail.com>
2024-10-08 16:35:52 +0900

    ADD : Add Transcription Field in Recording Model (#12) Add STT Transcription Function in RecordingModel.kt

--- app/src/main/java/org/linphone/ui/main/recordings/model/RecordingModel.kt
+++ app/src/main/java/org/linphone/ui/main/recordings/model/RecordingModel.kt
@@ -1,31 +1,16 @@
-/*
- * Copyright (c) 2010-2024 Belledonne Communications SARL.
- *
- * This file is part of linphone-android
- * (see https://www.linphone.org).
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
 package org.linphone.ui.main.recordings.model
 
 import androidx.annotation.UiThread
 import androidx.annotation.WorkerThread
+import java.io.File
 import java.text.SimpleDateFormat
 import java.util.Locale
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.linphone.LinphoneApplication.Companion.coreContext
 import org.linphone.core.Factory
 import org.linphone.core.tools.Log
+import org.linphone.utils.AIUtils
 import org.linphone.utils.FileUtils
 import org.linphone.utils.LinphoneUtils
 import org.linphone.utils.TimestampUtils
@@ -40,66 +25,58 @@ class RecordingModel @WorkerThread constructor(
     }
 
     val sipUri: String
-
     val displayName: String
-
     val timestamp: Long
-
     val month: String
-
     val dateTime: String
-
     val formattedDuration: String
-
     val duration: Int
+
+    // Transcription 저장
+    var transcription: String = ""
 
     init {
         if (isLegacy) {
+            // 레거시 파일 처리
             val username = fileName.split("_")[0]
             val sipAddress = coreContext.core.interpretUrl(username, false)
             sipUri = sipAddress?.asStringUriOnly() ?: username
-            displayName = if (sipAddress != null) {
-                val contact = coreContext.contactsManager.findContactByAddress(sipAddress)
-                contact?.name ?: LinphoneUtils.getDisplayName(sipAddress)
-            } else {
-                sipUri
-            }
+            displayName = sipAddress?.let {
+                val contact = coreContext.contactsManager.findContactByAddress(it)
+                contact?.name ?: LinphoneUtils.getDisplayName(it)
+            } ?: sipUri
 
             val parsedDate = fileName.split("_")[1]
-            var parsedTimestamp = 0L
-            try {
+            timestamp = try {
                 val date = SimpleDateFormat("dd-MM-yyyy-HH-mm-ss", Locale.getDefault()).parse(
                     parsedDate
                 )
-                parsedTimestamp = date?.time ?: 0L
+                date?.time ?: 0L
             } catch (e: Exception) {
                 Log.e("$TAG Failed to parse legacy timestamp [$parsedDate]")
+                0L
             }
-            timestamp = parsedTimestamp
         } else {
+            // 새로운 형식의 파일 처리
             val withoutHeader = fileName.substring(LinphoneUtils.RECORDING_FILE_NAME_HEADER.length)
             val indexOfSeparator = withoutHeader.indexOf(
                 LinphoneUtils.RECORDING_FILE_NAME_URI_TIMESTAMP_SEPARATOR
             )
             sipUri = withoutHeader.substring(0, indexOfSeparator)
             val sipAddress = Factory.instance().createAddress(sipUri)
-            displayName = if (sipAddress != null) {
-                val contact = coreContext.contactsManager.findContactByAddress(sipAddress)
-                contact?.name ?: LinphoneUtils.getDisplayName(sipAddress)
-            } else {
-                sipUri
-            }
+            displayName = sipAddress?.let {
+                val contact = coreContext.contactsManager.findContactByAddress(it)
+                contact?.name ?: LinphoneUtils.getDisplayName(it)
+            } ?: sipUri
 
             val parsedTimestamp = withoutHeader.substring(
                 indexOfSeparator + LinphoneUtils.RECORDING_FILE_NAME_URI_TIMESTAMP_SEPARATOR.length,
                 withoutHeader.lastIndexOf(".")
             )
-            Log.i(
-                "$TAG Extract SIP URI [$sipUri] and timestamp [$parsedTimestamp] from file [$fileName]"
-            )
             timestamp = parsedTimestamp.toLong()
         }
 
+        // 날짜 및 시간 정보 설정
         month = TimestampUtils.month(timestamp, timestampInSecs = false)
         val date = TimestampUtils.toString(
             timestamp,
@@ -110,6 +87,7 @@ class RecordingModel @WorkerThread constructor(
         val time = TimestampUtils.timeToString(timestamp, timestampInSecs = false)
         dateTime = "$date - $time"
 
+        // 녹음 파일의 재생 시간 처리
         val audioPlayer = coreContext.core.createLocalPlayer(null, null, null)
         if (audioPlayer != null) {
             audioPlayer.open(filePath)
@@ -121,9 +99,46 @@ class RecordingModel @WorkerThread constructor(
         }
     }
 
+    // 전사 기능
+    // 전사 기능
+    @UiThread
+    suspend fun transcribeRecording() {
+        try {
+            Log.i("$TAG Generating transcription for [$filePath]")
+            transcription = AIUtils.generateText(filePath)
+            Log.i("$TAG Transcription complete: $transcription")
+
+            // 전사된 텍스트 파일로 저장
+            val transcriptionFilePath = filePath.replace(".wav", "_transcription.txt")
+            val transcriptionFile = FileUtils.getFileStoragePath(
+                fileName = transcriptionFilePath,
+                isRecording = true,
+                overrideExisting = true
+            )
+            val success = FileUtils.dumpStringToFile(transcription, transcriptionFile)
+
+            if (success) {
+                Log.i("$TAG Transcription saved at: ${transcriptionFile.absolutePath}")
+            } else {
+                Log.e("$TAG Failed to save transcription at: ${transcriptionFile.absolutePath}")
+            }
+        } catch (e: Exception) {
+            Log.e("$TAG Failed to transcribe recording: ${e.message}")
+        }
+    }
+
+    // 녹음 파일 삭제
     @UiThread
     suspend fun delete() {
         Log.i("$TAG Deleting call recording [$filePath]")
-        FileUtils.deleteFile(filePath)
+        withContext(Dispatchers.IO) {
+            val file = File(filePath)
+            if (file.exists()) {
+                file.delete()
+                Log.i("$TAG Deleted [$filePath]")
+            } else {
+                Log.e("$TAG File [$filePath] does not exist")
+            }
+        }
     }
 }
